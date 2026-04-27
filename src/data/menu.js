1aa4c672a4e958cc9ff1020b91dc5001f5688d52
Hels <helsingrpp2@gmail.com>
2026-04-27 11:32:45 -0400

    Layout base
    wawawa ekisde no hay nada todavia pero ya esta la base hecha segun la pagina de JotaJota

---
+++ src/data/menu.js
@@ -0,0 +1,30 @@
+// src/data/menu.js
+const MENU_DATA = [
+    {
+        id: "Algo",
+        name: "ALGO EKISDE",
+        items: [
+            {name:"WAWAWA",
+            descripcion: "ES UN EKISDE DE EKISDE",
+            price: 50000000050,
+            currency: "CUP",
+            popular: false,
+            soldOut: false,
+        },     
+        ],
+    },
+    {
+        id: "cremas",
+        name: "creams ekisde",
+        items: [
+            {
+                name: "Creama con sabpr a queso humano EKisde",
+                descripcion: "Paja humana o no humana",
+                price: 200000002020202020202220,
+                currency: "CUP",
+                popular: true,
+                soldOut: false,
+            },
+        ],
+    },
+];
